889f033e8085bf435218e498302cdb5087b4af04
HouseMech <liam.harbec@gmail.com>
2025-08-06 11:43:37 -0700

    Started making cell class to make current tests pass

---
+++ lib/cell.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# A cell from conway's game of life. A cell is either dead or alive and changes states
+# based on the number of live or dead neighbour cells it has.
+class Cell
+  attr_accessor :x_coord, :y_coord, :alive, :board
+
+  def initialize(x,y,board)
+    @x_coord = x
+    @y_coord = y
+    @alive = false
+    @board = board
+  end
+end
