3f8f8dd0a96b160f67f03cdc51e0a60e7cd9d11c
HouseMech <liam.harbec@gmail.com>
2025-08-06 11:56:45 -0700

    Added new Cell tests

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require_relative '../lib/cell'
-
+# rubocop:disable Metrics/BlockLength
 RSpec.describe 'Cell' do
   let(:board) { double(:board) }
   let(:cell) { Cell.new(50, 50, board) }
@@ -15,5 +15,24 @@ RSpec.describe 'Cell' do
     it 'is either alive or dead' do
       expect(([true, false].include? cell.alive)).to eql true
     end
+
+    it 'can die' do
+      cell.die!
+      expect(cell.alive).to eql false
+    end
+
+    it 'can rise from the grave!' do
+      cell.live!
+      expect(cell.alive).to eql true
+    end
+  end
+
+  describe 'Game Logic' do
+    it 'has neighbours' do
+      results = double
+      expect(board).to receive(:neighbours).with(50, 50).and_return(results)
+      cell.neighbours
+    end
   end
 end
+# rubocop:enable Metrics/BlockLength
