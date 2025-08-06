41f7cd07a1b2e002519356d72dff8c205ba91b6e
HouseMech <liam.harbec@gmail.com>
2025-08-06 13:02:59 -0700

    Added a spec to check that we don't allow out of bound cells. Fixed some improper usage of ruby function calls

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -20,10 +20,15 @@ RSpec.describe 'Board' do
       expect(cell.y_coord).to eq 1
     end
 
+    it 'does not return a cell that is out of bounds' do
+      cell = board.cell_at(100, 100)
+      expect(cell).to eq nil
+    end
+
     it 'can return the neighbours of a given cell' do
-      neighbours = board.neighbours_of(0,1)
+      neighbours = board.neighbours_of(0, 1)
       expect(neighbours).to be_kind_of(Array)
-      expect(neighbours.!empty && neighbours.length <= 8)
+      expect(!neighbours.empty? && neighbours.length <= 8)
     end
   end
 end
